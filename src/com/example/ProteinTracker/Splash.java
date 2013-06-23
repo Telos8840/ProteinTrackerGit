b7ad0c1067cde9b0e93503dfe044b8a586330290
Matthew Roesener <roesenerm@gmail.com>
2013-06-23 11:50:57 -0700

    Tutorial

---
+++ src/com/example/ProteinTracker/Splash.java
@@ -0,0 +1,41 @@
+package com.example.ProteinTracker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: matthewroesener
+ * Date: 6/22/13
+ * Time: 4:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Splash extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle TravisLoveBacon){
+
+        super.onCreate(TravisLoveBacon);
+        setContentView(R.layout.splash);
+        Thread timer = new Thread(){
+
+            public void run(){
+                try{
+                    sleep(5000);
+                }  catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent openStartingPoint = new Intent("com.example.ProteinTracker.MyActivity");
+                    startActivity(openStartingPoint);
+                }
+            }
+
+        };
+        timer.start();
+    }
+
+
+
+}
